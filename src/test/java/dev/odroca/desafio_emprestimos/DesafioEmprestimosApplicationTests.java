4f56f98eebcebc60c83be88597734a6780a33e97
oDroca <davirochatz@gmail.com>
2025-12-14 15:22:30 -0300

    Inicio do desafio

---
+++ src/test/java/dev/odroca/desafio_emprestimos/DesafioEmprestimosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.odroca.desafio_emprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEmprestimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
